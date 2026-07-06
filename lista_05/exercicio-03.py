8cc2491d5a74a810cdf3ab9afea8cfe4840f83b2
Davi <davileitemarques@gmail.com>
2026-07-06 13:21:01 -0300

    Envio de casa para o Senai

---
+++ lista_05/exercicio-03.py
@@ -0,0 +1,16 @@
+c_empregado = int(input("Digite o seu codigo de empregado: "))
+ano_nasc = int(input("Digite o seu ano de nascimento: "))
+ingresso = int(input("Digite o seu ano de ingresso na empresa: "))
+
+ano_atual = 2026
+idade = ano_atual - ano_nasc
+tempo_trabalhado = ano_atual - ingresso
+
+print(f"Seu codigo: {c_empregado}")
+print(f"Sua idade é: {idade} anos")
+print(f"Seu tempo na empresa é de: {tempo_trabalhado} anos")
+
+if idade >= 65 or tempo_trabalhado >= 30 or (idade >= 60 and tempo_trabalhado >= 25):
+    print("Requerer aposentadoria!!")
+else:
+    print("Não requerer aposentadoria!!")
